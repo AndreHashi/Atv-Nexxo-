25b7137805154b6240db1d1be1633a20f49aeac1
AndreHashi <andrericardo0001@hotmail.com>
2025-04-25 09:34:45 -0300

    primeiro commit

---
+++ aula 1 - atv1.py
@@ -0,0 +1,7 @@
+nome = input("Escreva seu nome ou apelido: ")
+idade = input("Escreva sua suposta de idade: ")
+altura = input("Escreva sua altura: ")
+
+print(nome, "tem", idade, 'e de altura', altura)
+
+
---
+++ aula 2 - atv1.py
@@ -0,0 +1,16 @@
+nome = input("Coloque seu nome: ") 
+cpf = input("Informe seu CPF: ") 
+endereço = input("Endereço:") 
+idade = input("Insira sua idade: ") 
+altura = input("Insira sua altura: ") 
+telefone = input("Insira seu telefone: ") 
+
+print(
+    "Nome: " + nome,
+    "CPF: " + cpf,
+    "Idade: " + idade,
+    "Altura: " + str(altura),
+    "Telefone: " + telefone,
+    sep="\n"
+)
+
